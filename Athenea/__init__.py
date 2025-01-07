fabc03f01f0c2071be5c9148ee8010555b42678b
Fredy <fredyriveraacevedo13@gmail.com>
2025-01-07 04:14:46 +0000

    Update Test and README

--- Athenea/__init__.py
+++ Athenea/__init__.py
@@ -1,4 +1,8 @@
-""" Repo based implementation: https://github.com/VachanVY/Transfusion.torch """
+""" Repo based implementation: https://github.com/VachanVY/Transfusion.torch 
+
+And also based on the implementation of Llama 3 in the repo: https://github.com/Rivera-ai/ZeusLLM/blob/main/llm.py
+
+"""
 
 from .configs import MNIST_config, FashionMNIST_config
 from .diffusion_utils import DiffusionUtils
